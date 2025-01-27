a32aa8e31a2a90b69a704b2b7ef76b6c08163a85
Mehdi Fracso <mehdi.fracso@gemography.com>
2025-01-27 22:23:58 +0100

    Add Docker and environment configuration for development

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './bootstrap-module/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(process.env.BACKEND_PORT ?? 5000);
 }
 bootstrap();
